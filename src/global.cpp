99a0af2a2fe60f7214e230f19595526db84ea6ff
Alexander Elfimow <my.free.mail@gmx.de>
2017-02-27 23:39:09 +0100

    Added keyword global

---
+++ src/global.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "global.h"
+
+Keyword_global::Keyword_global()
+{
+}
+
+void Keyword_global::operator()(const std::string &name)
+{
+    std::cout << "global " << name << std::endl;
+}
+
+Keyword_global::~Keyword_global()
+{
+}

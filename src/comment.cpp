79a37610b21dedfba8c9ed91bb9d796d64b5eba6
Alexander Elfimow <my.free.mail@gmx.de>
2017-03-10 22:49:29 +0100

    Minor fix for printing comments

--- src/comment.cpp
+++ src/comment.cpp
@@ -8,7 +8,7 @@ Keyword_comment::Keyword_comment()
 
 void Keyword_comment::operator()(const std::string &str)
 {
-    std::cout << "#" << str << std::endl;
+    std::cout << "# " << str << std::endl;
 }
 
 Keyword_comment::~Keyword_comment()

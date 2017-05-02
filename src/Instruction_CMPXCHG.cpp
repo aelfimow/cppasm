cb279c9cc44e8be0f085a3a07e3fd878a934a8e5
Alexander Elfimow <my.free.mail@gmx.de>
2017-05-02 21:41:03 +0200

    Added class for CMPXCHG instruction

---
+++ src/Instruction_CMPXCHG.cpp
@@ -0,0 +1,73 @@
+#include <cstdint>
+#include <string>
+#include "Instruction.h"
+#include "asmstream.h"
+#include "reg.h"
+#include "r8.h"
+#include "r16.h"
+#include "r32.h"
+#include "r64.h"
+#include "m.h"
+#include "m8.h"
+#include "m16.h"
+#include "m32.h"
+#include "m64.h"
+#include "Instruction_CMPXCHG.h"
+
+Instruction_CMPXCHG::Instruction_CMPXCHG(asmstream &s) :
+    m_asmout { s },
+    m_mnem { "cmpxchg" }
+{
+}
+
+void Instruction_CMPXCHG::operator()(const r8 &op1, const r8 &op2)
+{
+    Instruction instr { m_mnem, op1.name(), op2.name() };
+    m_asmout << instr;
+}
+
+void Instruction_CMPXCHG::operator()(const m8 &op1, const r8 &op2)
+{
+    Instruction instr { m_mnem, op1.to_str(), op2.name() };
+    m_asmout << instr;
+}
+
+void Instruction_CMPXCHG::operator()(const r16 &op1, const r16 &op2)
+{
+    Instruction instr { m_mnem, op1.name(), op2.name() };
+    m_asmout << instr;
+}
+
+void Instruction_CMPXCHG::operator()(const m16 &op1, const r16 &op2)
+{
+    Instruction instr { m_mnem, op1.to_str(), op2.name() };
+    m_asmout << instr;
+}
+
+void Instruction_CMPXCHG::operator()(const r32 &op1, const r32 &op2)
+{
+    Instruction instr { m_mnem, op1.name(), op2.name() };
+    m_asmout << instr;
+}
+
+void Instruction_CMPXCHG::operator()(const m32 &op1, const r32 &op2)
+{
+    Instruction instr { m_mnem, op1.to_str(), op2.name() };
+    m_asmout << instr;
+}
+
+void Instruction_CMPXCHG::operator()(const r64 &op1, const r64 &op2)
+{
+    Instruction instr { m_mnem, op1.name(), op2.name() };
+    m_asmout << instr;
+}
+
+void Instruction_CMPXCHG::operator()(const m64 &op1, const r64 &op2)
+{
+    Instruction instr { m_mnem, op1.to_str(), op2.name() };
+    m_asmout << instr;
+}
+
+Instruction_CMPXCHG::~Instruction_CMPXCHG()
+{
+}

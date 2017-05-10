70f8bb8096528ce91ce694a6040623f4f2ae4dbd
Alexander Elfimow <my.free.mail@gmx.de>
2017-05-10 22:49:26 +0200

    Added base index scale and displacement support to m64

--- src/m64.cpp
+++ src/m64.cpp
@@ -4,37 +4,112 @@
 #include "m.h"
 #include "m64.h"
 
-m64::m64(const reg &base) :
-    m(),
-    m_str { "" }
+m64::m64() :
+    m {},
+    m_base { "" },
+    m_index { "" },
+    m_scale { "" },
+    m_disp { "" }
 {
-    m_str = "(" + base.name() + ")";
+}
+
+m64::m64(const reg &base) :
+    m {},
+    m_base { base.name() },
+    m_index { "" },
+    m_scale { "" },
+    m_disp { "" }
+{
 }
 
 m64::m64(const reg &base, const reg &index) :
     m(),
-    m_str { "" }
+    m_base { base.name() },
+    m_index { index.name() },
+    m_scale { "" },
+    m_disp { "" }
 {
-    m_str = "(" + base.name() + ", " + index.name() + ")";
 }
 
 m64::m64(const reg &base, const reg &index, size_t scale) :
     m(),
-    m_str { "" }
+    m_base { base.name() },
+    m_index { index.name() },
+    m_scale { "" },
+    m_disp { "" }
 {
     std::ostringstream ss;
     ss << scale;
-
-    m_str = "(" + base.name() + ", " + index.name() + ", " + ss.str() + ")";
+    m_scale = ss.str();
 }
 
 m64::~m64()
 {
 }
 
+m64 &m64::base(const reg &b)
+{
+    m_base = b.name();
+    return *this;
+}
+
+m64 &m64::index(const reg &i)
+{
+    m_index = i.name();
+    return *this;
+}
+
+m64 &m64::scale(size_t value)
+{
+    std::ostringstream ss;
+    ss << value;
+    m_scale = ss.str();
+    return *this;
+}
+
+m64 &m64::disp(size_t value)
+{
+    if (0 == value)
+    {
+        m_disp.clear();
+    }
+    else
+    {
+        std::ostringstream ss;
+        ss << value;
+        m_disp = ss.str();
+    }
+    return *this;
+}
+
 std::string m64::to_str() const
 {
-    return m_str;
+    std::string str;
+
+    str.append("(");
+    str.append(m_base);
+
+    if (0 != m_index.length())
+    {
+        str.append(", " + m_index);
+
+        if (0 != m_scale.length())
+        {
+            str.append(", " + m_scale);
+        }
+    }
+    else
+    {
+        // No index register, expect scale to be empty, if not, error
+        if (0 != m_scale.length())
+        {
+            throw std::logic_error("No index register, but scale is set");
+        }
+    }
+
+    str.append(")");
+
+    return { m_disp + str };
 }
 
 std::string m64::postfix() const
--- src/m64.h
+++ src/m64.h
@@ -6,11 +6,11 @@
 class m64 : public m
 {
     public:
+        m64();
         explicit m64(const reg &base);
         explicit m64(const reg &base, const reg &index);
         explicit m64(const reg &base, const reg &index, size_t scale);
 
-        m64() = delete;
         m64(const m64 &instance) = delete;
         m64(const m64 &&instance) = delete;
         m64 &operator=(const m64 &instance) = delete;
@@ -18,11 +18,19 @@ class m64 : public m
 
         ~m64();
 
+        m64 &base(const reg &b);
+        m64 &index(const reg &i);
+        m64 &scale(size_t value);
+        m64 &disp(size_t value);
+
         std::string to_str() const override;
         std::string postfix() const override;
 
     private:
-        std::string m_str;
+        std::string m_base;
+        std::string m_index;
+        std::string m_scale;
+        std::string m_disp;
 };
 
 #endif

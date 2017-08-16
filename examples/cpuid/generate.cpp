5d166c4064803432983113d6f4993265d4ab62e6
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-16 22:17:29 +0200

    cpuid example: added generator function for Linux

--- examples/cpuid/generate.cpp
+++ examples/cpuid/generate.cpp
@@ -4,7 +4,7 @@
 
 #include "cppasm.h"
 
-static void gen_Windows()
+static void gen_WindowsFunc()
 {
     // Function name to be generated
     const std::string func_name { "execute_cpuid" };
@@ -52,13 +52,84 @@ static void gen_Windows()
     RET();
 }
 
+static void gen_LinuxFunc()
+{
+    // Function name to be generated
+    const std::string func_name { "execute_cpuid" };
+    comment("void " + func_name + "(size_t RAX_value, size_t RCX_value, size_t *pOut)");
+    comment("RAX_value is in %rdi");
+    comment("RCX_value is in %rsi");
+    comment("pOut is in %rdx");
+
+    global(func_name);
+
+    section code { ".text" };
+    code.start();
+
+    label(func_name);
+
+    r64 &reg_to_save = RBX;
+    PUSH(reg_to_save);  // this register must be saved (and restored later)
+
+    MOV(R8, RDX);   // move pOut, because RDX will be overwritten by CPUID
+    MOV(RAX, RDI);  // RAX_value
+    MOV(RCX, RSI);  // RCX_value
+
+    CPUID();
+
+    size_t offset { 0 };
+    size_t step { 8 };
+    m64 buffer { R8 };
+
+    buffer.disp(offset);
+    MOV(buffer, RAX);
+
+    offset += step;
+    buffer.disp(offset);
+    MOV(buffer, RBX);
+
+    offset += step;
+    buffer.disp(offset);
+    MOV(buffer, RCX);
+
+    offset += step;
+    buffer.disp(offset);
+    MOV(buffer, RDX);
+
+    POP(reg_to_save);   // restore register
+
+    RET();
+}
+
 int main(int argc, char *argv[])
 try
 {
-    argc = argc;
-    argv = argv;
+    const std::string usageStr { "Usage: generate windows|linux" };
 
-    gen_Windows();
+    if (argc != 2)
+    {
+        throw std::invalid_argument(usageStr);
+    }
+
+    std::string os { argv[1] };
+
+    bool forWindows { os == "windows" };
+    bool forLinux { os == "linux" };
+
+    if (!forWindows && !forLinux)
+    {
+        throw std::invalid_argument(usageStr);
+    }
+
+    if (forWindows)
+    {
+        gen_WindowsFunc();
+    }
+
+    if (forLinux)
+    {
+        gen_LinuxFunc();
+    }
 
     return EXIT_SUCCESS;
 }

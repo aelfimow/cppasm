f150d505320b80e0487d0dc12e921a577cb76493
Alexander Elfimow <my.free.mail@gmx.de>
2017-10-06 23:30:34 +0200

    Minor class layout change

--- src/label.h
+++ src/label.h
@@ -8,14 +8,15 @@ class Keyword_label
     public:
         Keyword_label();
 
+        void operator()(const std::string &name);
+
+        ~Keyword_label();
+
+    public:
         Keyword_label(const Keyword_label &instance) = delete;
         Keyword_label(const Keyword_label &&instance) = delete;
         Keyword_label &operator=(const Keyword_label &instance) = delete;
         Keyword_label &operator=(const Keyword_label &&instance) = delete;
-
-        void operator()(const std::string &name);
-
-        ~Keyword_label();
 };
 
 #endif

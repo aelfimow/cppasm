b36075514e4f3bbfd29e01660d1325e7b9319225
Alexander Elfimow <my.free.mail@gmx.de>
2017-10-06 23:28:36 +0200

    Minor class layout change

--- src/cr.h
+++ src/cr.h
@@ -8,18 +8,19 @@ class cr
     public:
         explicit cr(const std::string &name);
 
-        cr() = delete;
-        cr(const cr &instance) = delete;
-        cr(const cr &&instance) = delete;
-        cr &operator=(const cr &instance) = delete;
-        cr &operator=(const cr &&instance) = delete;
-
         ~cr();
 
         std::string name() const;
 
     private:
         const std::string m_name;
+
+    public:
+        cr() = delete;
+        cr(const cr &instance) = delete;
+        cr(const cr &&instance) = delete;
+        cr &operator=(const cr &instance) = delete;
+        cr &operator=(const cr &&instance) = delete;
 };
 
 #endif

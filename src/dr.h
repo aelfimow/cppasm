c434f6efd69b487f4ea57fbdea8cac851c14d765
Alexander Elfimow <my.free.mail@gmx.de>
2017-10-06 23:29:14 +0200

    Minor class layout change

--- src/dr.h
+++ src/dr.h
@@ -8,18 +8,19 @@ class dr
     public:
         explicit dr(const std::string &name);
 
-        dr() = delete;
-        dr(const dr &instance) = delete;
-        dr(const dr &&instance) = delete;
-        dr &operator=(const dr &instance) = delete;
-        dr &operator=(const dr &&instance) = delete;
-
         ~dr();
 
         std::string name() const;
 
     private:
         const std::string m_name;
+
+    public:
+        dr() = delete;
+        dr(const dr &instance) = delete;
+        dr(const dr &&instance) = delete;
+        dr &operator=(const dr &instance) = delete;
+        dr &operator=(const dr &&instance) = delete;
 };
 
 #endif

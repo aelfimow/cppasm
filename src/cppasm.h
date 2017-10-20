a11b0ba275b896abfcf70991e91be570beb4af46
Alexander Elfimow <my.free.mail@gmx.de>
2017-10-20 21:52:29 +0200

    Use new header file

--- src/cppasm.h
+++ src/cppasm.h
@@ -44,6 +44,7 @@ extern const std::string suffix_m64int;
 #include "Instruction_x.h"
 #include "Instruction1_Types.h"
 #include "Instruction2_Types.h"
+#include "Instruction3_Types.h"
 
 using Instruction_MOVD = Instruction_8
 <
@@ -101,27 +102,6 @@ using Instruction_MMX_Shift = Instruction_6
     Operands_reg_mem<xmm, m128, no_suffix>,
     Operands_reg_imm<mm, imm8>,
     Operands_reg_imm<xmm, imm8>
->;
-
-using Instruction_LoadFarPointer = Instruction_3
-<
-    Operands_reg_mem<r16, m, no_suffix>,
-    Operands_reg_mem<r32, m, no_suffix>,
-    Operands_reg_mem<r64, m, no_suffix>
->;
-
-using Instruction_LEA = Instruction_3
-<
-    Operands_reg_mem<r16, m, no_suffix>,
-    Operands_reg_mem<r32, m, no_suffix>,
-    Operands_reg_mem<r64, m, no_suffix>
->;
-
-using Instruction_RDRAND = Instruction_3
-<
-    Operands_reg<r16>,
-    Operands_reg<r32>,
-    Operands_reg<r64>
 >;
 
 using Instruction_Arithm1 = Instruction_26
@@ -245,20 +225,6 @@ using Instruction_Branch = Instruction_7
     Operands_mem<m32, no_suffix>,
     Operands_reg<r64>,
     Operands_mem<m64, no_suffix>
->;
-
-using Instruction_SSE_MOV1 = Instruction_3
-<
-    Operands_reg_reg<xmm, xmm, no_suffix>,
-    Operands_reg_mem<xmm, m128, no_suffix>,
-    Operands_mem_reg<m128, xmm, no_suffix>
->;
-
-using Instruction_SSE_MOV5 = Instruction_3
-<
-    Operands_reg_reg<xmm, xmm, no_suffix>,
-    Operands_reg_mem<xmm, m32, no_suffix>,
-    Operands_mem_reg<m32, xmm, no_suffix>
 >;
 
 using Instruction_SSE_Conv2 = Instruction_4
@@ -465,40 +431,12 @@ using Instruction_FLD = Instruction_4
     Operands_reg<st>
 >;
 
-using Instruction_FILD = Instruction_3
-<
-    Operands_mem<m16, suffix_m16int>,
-    Operands_mem<m32, suffix_m32int>,
-    Operands_mem<m64, suffix_m64int>
->;
-
-using Instruction_FISTP = Instruction_3
-<
-    Operands_mem<m16, suffix_m16int>,
-    Operands_mem<m32, suffix_m32int>,
-    Operands_mem<m64, suffix_m64int>
->;
-
-using Instruction_FST = Instruction_3
-<
-    Operands_mem<m32fp, suffix_m32fp>,
-    Operands_mem<m64fp, suffix_m64fp>,
-    Operands_reg<st>
->;
-
 using Instruction_FSTP = Instruction_4
 <
     Operands_mem<m32fp, suffix_m32fp>,
     Operands_mem<m64fp, suffix_m64fp>,
     Operands_mem<m80fp, suffix_m80fp>,
     Operands_reg<st>
->;
-
-using Instruction_FPU_Arithm1 = Instruction_3
-<
-    Operands_mem<m32fp, suffix_m32fp>,
-    Operands_mem<m64fp, suffix_m64fp>,
-    Operands_reg_reg<st, st, no_suffix>
 >;
 
 using Instruction_FCOM = Instruction_4
@@ -574,20 +512,6 @@ using Instruction_PSADBW = Instruction_4
     Operands_reg_mem<xmm, m128, no_suffix>
 >;
 
-using Instruction_MOVxPD1 = Instruction_3
-<
-    Operands_reg_reg<xmm, xmm, no_suffix>,
-    Operands_reg_mem<xmm, m128, no_suffix>,
-    Operands_mem_reg<m128, xmm, no_suffix>
->;
-
-using Instruction_MOVSD = Instruction_3
-<
-    Operands_reg_reg<xmm, xmm, no_suffix>,
-    Operands_reg_mem<xmm, m64, no_suffix>,
-    Operands_mem_reg<m64, xmm, no_suffix>
->;
-
 using Instruction_CVTSD2SI = Instruction_4
 <
     Operands_reg_reg<r32, xmm, no_suffix>,
@@ -604,13 +528,6 @@ using Instruction_CVTSI2SD = Instruction_4
     Operands_reg_mem<xmm, m64, no_suffix>
 >;
 
-using Instruction_MOVDQx = Instruction_3
-<
-    Operands_reg_reg<xmm, xmm, no_suffix>,
-    Operands_reg_mem<xmm, m128, no_suffix>,
-    Operands_mem_reg<m128, xmm, no_suffix>
->;
-
 using Instruction_PMULUDQ = Instruction_4
 <
     Operands_reg_reg<mm, mm, no_suffix>,
@@ -625,13 +542,6 @@ using Instruction_SSE2_Int_Arithm1 = Instruction_4
     Operands_reg_mem<mm, m64, no_suffix>,
     Operands_reg_reg<xmm, xmm, no_suffix>,
     Operands_reg_mem<xmm, m128, no_suffix>
->;
-
-using Instruction_FISTTP = Instruction_3
-<
-    Operands_mem<m16, suffix_m16int>,
-    Operands_mem<m32, suffix_m32int>,
-    Operands_mem<m64, suffix_m64int>
 >;
 
 using Instruction_SSSE3_Arithm1 = Instruction_4
@@ -656,27 +566,6 @@ using Instruction_SSE4_1_Type3 = Instruction_4
     Operands_reg_mem<xmm, m128, no_suffix>,
     Operands_reg_reg_reg<xmm, xmm, xmm>,
     Operands_reg_mem_reg<xmm, m128, xmm>
->;
-
-using Instruction_SSE4_1_Type6 = Instruction_3
-<
-    Operands_reg_reg_imm<r32, xmm, imm8>,
-    Operands_reg_reg_imm<r64, xmm, imm8>,
-    Operands_mem_reg_imm<m32, xmm, imm8>
->;
-
-using Instruction_SSE4_1_Type11 = Instruction_3
-<
-    Operands_reg_reg_imm<r32, xmm, imm8>,
-    Operands_reg_reg_imm<r64, xmm, imm8>,
-    Operands_mem_reg_imm<m8, xmm, imm8>
->;
-
-using Instruction_SSE4_1_Type12 = Instruction_3
-<
-    Operands_reg_reg_imm<r32, xmm, imm8>,
-    Operands_reg_reg_imm<r64, xmm, imm8>,
-    Operands_mem_reg_imm<m32, xmm, imm8>
 >;
 
 using Instruction_F16C_Type1 = Instruction_4

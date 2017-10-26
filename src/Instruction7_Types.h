6adbea10517375e9221ce126bc186c95369ed40b
Alexander Elfimow <my.free.mail@gmx.de>
2017-10-27 00:04:52 +0200

    Added header file

---
+++ src/Instruction7_Types.h
@@ -0,0 +1,31 @@
+/*! \file Instruction7_Types.h
+    \brief Types for instructions with seven operands combinations.
+*/
+#ifdef INSTRUCTION7_TYPES_H
+#error Already included
+#else
+#define INSTRUCTION7_TYPES_H
+
+using Instruction_Branch = Instruction_7
+<
+    Operands_string,
+    Operands_reg<r16>,
+    Operands_mem<m16, no_suffix>,
+    Operands_reg<r32>,
+    Operands_mem<m32, no_suffix>,
+    Operands_reg<r64>,
+    Operands_mem<m64, no_suffix>
+>;
+
+using Instruction_POP = Instruction_7
+<
+    Operands_reg<r16>,
+    Operands_mem<m16, suffix_m16>,
+    Operands_reg<r32>,
+    Operands_mem<m32, suffix_m32>,
+    Operands_reg<r64>,
+    Operands_mem<m64, suffix_m64>,
+    Operands_reg<Sreg>
+>;
+
+#endif

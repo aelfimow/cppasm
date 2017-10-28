7c6155843b944a9c04ce1309dcce11ebec96797c
Alexander Elfimow <my.free.mail@gmx.de>
2017-10-28 22:34:49 +0200

    Use new header file

--- src/cppasm.h
+++ src/cppasm.h
@@ -51,6 +51,7 @@ extern const std::string suffix_m64int;
 #include "Instruction7_Types.h"
 #include "Instruction8_Types.h"
 #include "Instruction10_Types.h"
+#include "Instruction12_Types.h"
 
 using Instruction_Arithm1 = Instruction_26
 <
@@ -79,38 +80,6 @@ using Instruction_Arithm1 = Instruction_26
     Operands_reg_mem<r8, m8, no_suffix>,
     Operands_reg_mem<r16, m16, no_suffix>,
     Operands_reg_mem<r32, m32, no_suffix>,
-    Operands_reg_mem<r64, m64, no_suffix>
->;
-
-using Instruction_BitTest = Instruction_12
-<
-    Operands_reg_reg<r16, r16, no_suffix>,
-    Operands_mem_reg<m16, r16, no_suffix>,
-    Operands_reg_reg<r32, r32, no_suffix>,
-    Operands_mem_reg<m32, r32, no_suffix>,
-    Operands_reg_reg<r64, r64, no_suffix>,
-    Operands_mem_reg<m64, r64, no_suffix>,
-    Operands_reg_imm<r16, imm8>,
-    Operands_reg_imm<r32, imm8>,
-    Operands_reg_imm<r64, imm8>,
-    Operands_mem_imm<m16, imm8, suffix_m16>,
-    Operands_mem_imm<m32, imm8, suffix_m32>,
-    Operands_mem_imm<m64, imm8, suffix_m64>
->;
-
-using Instruction_XCHG = Instruction_12
-<
-    Operands_reg_reg<r8, r8, no_suffix>,
-    Operands_mem_reg<m8, r8, no_suffix>,
-    Operands_reg_mem<r8, m8, no_suffix>,
-    Operands_reg_reg<r16, r16, no_suffix>,
-    Operands_mem_reg<m16, r16, no_suffix>,
-    Operands_reg_mem<r16, m16, no_suffix>,
-    Operands_reg_reg<r32, r32, no_suffix>,
-    Operands_mem_reg<m32, r32, no_suffix>,
-    Operands_reg_mem<r32, m32, no_suffix>,
-    Operands_reg_reg<r64, r64, no_suffix>,
-    Operands_mem_reg<m64, r64, no_suffix>,
     Operands_reg_mem<r64, m64, no_suffix>
 >;
 
@@ -172,38 +141,6 @@ using Instruction_MOV = Instruction_36
     Operands_reg_reg<r64, dr, no_suffix>,
     Operands_reg_reg<dr, r32, no_suffix>,
     Operands_reg_reg<dr, r64, no_suffix>
->;
-
-using Instruction_MovWithExt = Instruction_12
-<
-    Operands_reg_reg<r16, r8, suffix_bw>,
-    Operands_reg_mem<r16, m8, suffix_bw>,
-    Operands_reg_reg<r32, r8, suffix_bl>,
-    Operands_reg_mem<r32, m8, suffix_bl>,
-    Operands_reg_reg<r64, r8, suffix_bq>,
-    Operands_reg_mem<r64, m8, suffix_bq>,
-    Operands_reg_reg<r32, r16, suffix_wl>,
-    Operands_reg_mem<r32, m16, suffix_wl>,
-    Operands_reg_reg<r64, r16, suffix_wq>,
-    Operands_reg_mem<r64, m16, suffix_wq>,
-    Operands_reg_reg<r64, r32, suffix_lq>,
-    Operands_reg_mem<r64, m32, suffix_lq>
->;
-
-using Instruction_ShiftDouble = Instruction_12
-<
-    Operands_reg_reg_imm<r16, r16, imm8>,
-    Operands_mem_reg_imm<m16, r16, imm8>,
-    Operands_reg_reg_reg<r16, r16, r8>,
-    Operands_mem_reg_reg<m16, r16, r8>,
-    Operands_reg_reg_imm<r32, r32, imm8>,
-    Operands_mem_reg_imm<m32, r32, imm8>,
-    Operands_reg_reg_imm<r64, r64, imm8>,
-    Operands_mem_reg_imm<m64, r64, imm8>,
-    Operands_reg_reg_reg<r32, r32, r8>,
-    Operands_mem_reg_reg<m32, r32, r8>,
-    Operands_reg_reg_reg<r64, r64, r8>,
-    Operands_mem_reg_reg<m64, r64, r8>
 >;
 
 using Instruction_Shift = Instruction_16

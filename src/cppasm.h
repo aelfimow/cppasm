0f1cd3922e9ea169e982dd62c97606fde7f05ce7
Alexander Elfimow <my.free.mail@gmx.de>
2017-10-28 22:53:55 +0200

    Use new header file

--- src/cppasm.h
+++ src/cppasm.h
@@ -54,46 +54,7 @@ extern const std::string suffix_m64int;
 #include "Instruction12_Types.h"
 #include "Instruction16_Types.h"
 #include "Instruction26_Types.h"
-
-using Instruction_MOV = Instruction_36
-<
-    Operands_reg_reg<r8, r8, no_suffix>,
-    Operands_mem_reg<m8, r8, no_suffix>,
-    Operands_reg_reg<r16, r16, no_suffix>,
-    Operands_mem_reg<m16, r16, no_suffix>,
-    Operands_reg_reg<r32, r32, no_suffix>,
-    Operands_mem_reg<m32, r32, no_suffix>,
-    Operands_reg_reg<r64, r64, no_suffix>,
-    Operands_mem_reg<m64, r64, no_suffix>,
-    Operands_reg_mem<r8, m8, no_suffix>,
-    Operands_reg_mem<r16, m16, no_suffix>,
-    Operands_reg_mem<r32, m32, no_suffix>,
-    Operands_reg_mem<r64, m64, no_suffix>,
-    Operands_reg_reg<r16, Sreg, no_suffix>,
-    Operands_mem_reg<m16, Sreg, no_suffix>,
-    Operands_reg_reg<r64, Sreg, no_suffix>,
-    Operands_mem_reg<m64, Sreg, no_suffix>,
-    Operands_reg_reg<Sreg, r16, no_suffix>,
-    Operands_reg_mem<Sreg, m16, no_suffix>,
-    Operands_reg_reg<Sreg, r64, no_suffix>,
-    Operands_reg_mem<Sreg, m64, no_suffix>,
-    Operands_reg_imm<r8, imm8>,
-    Operands_reg_imm<r16, imm16>,
-    Operands_reg_imm<r32, imm32>,
-    Operands_reg_imm<r64, imm64>,
-    Operands_mem_imm<m8, imm8, suffix_m8>,
-    Operands_mem_imm<m16, imm16, suffix_m16>,
-    Operands_mem_imm<m32, imm32, suffix_m32>,
-    Operands_mem_imm<m64, imm32, suffix_m64>,
-    Operands_reg_reg<r32, cr, no_suffix>,
-    Operands_reg_reg<r64, cr, no_suffix>,
-    Operands_reg_reg<cr, r32, no_suffix>,
-    Operands_reg_reg<cr, r64, no_suffix>,
-    Operands_reg_reg<r32, dr, no_suffix>,
-    Operands_reg_reg<r64, dr, no_suffix>,
-    Operands_reg_reg<dr, r32, no_suffix>,
-    Operands_reg_reg<dr, r64, no_suffix>
->;
+#include "Instruction36_Types.h"
 
 extern asmstream asmout;
 

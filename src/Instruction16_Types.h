5b6880cb068ab9cce905e0c59cfe95b93321dcf5
Alexander Elfimow <my.free.mail@gmx.de>
2017-10-28 22:42:18 +0200

    Added header file

---
+++ src/Instruction16_Types.h
@@ -0,0 +1,49 @@
+/*! \file Instruction16_Types.h
+    \brief Types for instructions with 16 operands combinations.
+*/
+#ifdef INSTRUCTION16_TYPES_H
+#error Already included
+#else
+#define INSTRUCTION16_TYPES_H
+
+using Instruction_TEST = Instruction_16
+<
+    Operands_reg_imm<r8, imm8>,
+    Operands_mem_imm<m8, imm8, suffix_m8>,
+    Operands_reg_imm<r16, imm16>,
+    Operands_mem_imm<m16, imm16, suffix_m16>,
+    Operands_reg_imm<r32, imm32>,
+    Operands_mem_imm<m32, imm32, suffix_m32>,
+    Operands_reg_imm<r64, imm32>,
+    Operands_mem_imm<m64, imm32, suffix_m64>,
+    Operands_reg_reg<r8, r8, no_suffix>,
+    Operands_mem_reg<m8, r8, no_suffix>,
+    Operands_reg_reg<r16, r16, no_suffix>,
+    Operands_mem_reg<m16, r16, no_suffix>,
+    Operands_reg_reg<r32, r32, no_suffix>,
+    Operands_mem_reg<m32, r32, no_suffix>,
+    Operands_reg_reg<r64, r64, no_suffix>,
+    Operands_mem_reg<m64, r64, no_suffix>
+>;
+
+using Instruction_Shift = Instruction_16
+<
+    Operands_reg_imm<r8, imm8>,
+    Operands_mem_imm<m8, imm8, suffix_m8>,
+    Operands_reg_reg<r8, r8, no_suffix>,
+    Operands_mem_reg<m8, r8, suffix_m8>,
+    Operands_reg_imm<r16, imm8>,
+    Operands_mem_imm<m16, imm8, suffix_m16>,
+    Operands_reg_reg<r16, r8, no_suffix>,
+    Operands_mem_reg<m16, r8, suffix_m16>,
+    Operands_reg_imm<r32, imm8>,
+    Operands_mem_imm<m32, imm8, suffix_m32>,
+    Operands_reg_reg<r32, r8, no_suffix>,
+    Operands_mem_reg<m32, r8, suffix_m32>,
+    Operands_reg_imm<r64, imm8>,
+    Operands_mem_imm<m64, imm8, suffix_m64>,
+    Operands_reg_reg<r64, r8, no_suffix>,
+    Operands_mem_reg<m64, r8, suffix_m64>
+>;
+
+#endif

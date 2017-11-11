6d9e5f2265fb12b2f4ce733964371ce3e234e586
Alexander Elfimow <my.free.mail@gmx.de>
2017-11-11 22:00:47 +0100

    Revert "tests: added file"
    This reverts commit 3c9e6551004093ced56ec259f67c8071569d8ff6.

--- tests/common.h
+++
@@ -1,8 +0,0 @@
-#ifdef COMMON_H
-#error Already included
-#else
-
-extern void gen_Instruction1_Type1(Instruction1_Type1 &instr);
-extern void gen_Instruction1_Type2(Instruction1_Type2 &instr);
-
-#endif

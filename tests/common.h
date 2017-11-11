3c9e6551004093ced56ec259f67c8071569d8ff6
Alexander Elfimow <my.free.mail@gmx.de>
2017-11-11 21:45:41 +0100

    tests: added file

---
+++ tests/common.h
@@ -0,0 +1,8 @@
+#ifdef COMMON_H
+#error Already included
+#else
+
+extern void gen_Instruction1_Type1(Instruction1_Type1 &instr);
+extern void gen_Instruction1_Type2(Instruction1_Type2 &instr);
+
+#endif

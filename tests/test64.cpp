6864d6aca17307da8acac5dc2b77a7d19651cfc7
Alexander Elfimow <my.free.mail@gmx.de>
2017-11-20 22:22:27 +0100

    test64: added test for Instruction1_Type15

--- tests/test64.cpp
+++ tests/test64.cpp
@@ -91,6 +91,11 @@ static void gen_Instruction1_Type14(Instruction1_Type14 &instr)
     instr(addr);
 }
 
+static void gen_Instruction1_Type15(Instruction1_Type15 &instr)
+{
+    instr(MM1, MM0);
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -326,6 +331,8 @@ try
     gen_Instruction1_Type14(STMXCSR);
     gen_Instruction1_Type14(VLDMXCSR);
     gen_Instruction1_Type14(VSTMXCSR);
+
+    gen_Instruction1_Type15(MASKMOVQ);
 
     return EXIT_SUCCESS;
 }

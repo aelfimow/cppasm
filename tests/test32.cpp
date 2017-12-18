95075ca29d206fdbe9526aeea2ad76020b74cf7b
Alexander Elfimow <my.free.mail@gmx.de>
2017-12-18 22:17:07 +0100

    test32: added test for Instruction2_Type30

--- tests/test32.cpp
+++ tests/test32.cpp
@@ -356,6 +356,14 @@ static void gen_Instruction2_Type29(Instruction2_Type29 &instr)
     instr(XMM0, XMM1, addr);
 }
 
+static void gen_Instruction2_Type30(Instruction2_Type30 &instr)
+{
+    imm8 mask { 255 };
+    m128 addr { EAX };
+    instr(XMM0, XMM1, XMM2, mask);
+    instr(XMM0, XMM1, addr, mask);
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -872,6 +880,8 @@ try
     gen_Instruction2_Type29(VFNMSUB132SS);
     gen_Instruction2_Type29(VFNMSUB213SS);
     gen_Instruction2_Type29(VFNMSUB231SS);
+
+    gen_Instruction2_Type30(VDPPD);
 
     return EXIT_SUCCESS;
 }

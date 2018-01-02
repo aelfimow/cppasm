211aeb5fd6a7b5f669ad9ef3cad895d8883d92cf
Alexander Elfimow <my.free.mail@gmx.de>
2018-01-02 23:05:04 +0100

    test32: added test for Instruction4_Type19

--- tests/test32.cpp
+++ tests/test32.cpp
@@ -645,6 +645,14 @@ static void gen_Instruction4_Type18(Instruction4_Type18 &instr)
     instr(YMM0, addr2);
 }
 
+static void gen_Instruction4_Type19(Instruction4_Type19 &instr)
+{
+    instr(XMM0, XMM1);
+    //instr(XMM0, addr1);
+    instr(XMM0, YMM1);
+    //instr(XMM0, addr2);
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -1397,6 +1405,10 @@ try
     gen_Instruction4_Type18(VPTEST);
     gen_Instruction4_Type18(VTESTPS);
     gen_Instruction4_Type18(VTESTPD);
+
+    gen_Instruction4_Type19(VCVTPD2PS);
+    gen_Instruction4_Type19(VCVTTPD2DQ);
+    gen_Instruction4_Type19(VCVTPD2DQ);
 
     return EXIT_SUCCESS;
 }

0b1a686789416ad3ed84263e0cd20892ff5df6ac
Alexander Elfimow <my.free.mail@gmx.de>
2018-01-04 23:46:49 +0100

    test32: added test for Instruction5_Type2

--- tests/test32.cpp
+++ tests/test32.cpp
@@ -718,6 +718,15 @@ static void gen_Instruction5_Type1(Instruction5_Type1 &instr)
     instr(addr2);
 }
 
+static void gen_Instruction5_Type2(Instruction5_Type2 &instr)
+{
+    imm8 mask { 255 };
+    m16 addr { EAX };
+    instr(EAX, MM0, mask);
+    instr(EAX, XMM0, mask);
+    instr(addr, XMM0, mask);
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -1502,6 +1511,8 @@ try
     gen_Instruction4_Type25(VCVTSI2SD);
 
     gen_Instruction5_Type1(NOP);
+
+    gen_Instruction5_Type2(PEXTRW);
 
     return EXIT_SUCCESS;
 }

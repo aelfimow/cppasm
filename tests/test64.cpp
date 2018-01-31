836c763681e51a6baa97df639cc5bb6b2c7a8077
Alexander Elfimow <my.free.mail@gmx.de>
2018-01-31 21:05:58 +0100

    test64: added test for Instruction16_Type2

--- tests/test64.cpp
+++ tests/test64.cpp
@@ -1050,6 +1050,31 @@ static void gen_Instruction16_Type1(Instruction16_Type1 &instr)
     instr(addr4, RAX);
 }
 
+static void gen_Instruction16_Type2(Instruction16_Type2 &instr)
+{
+    imm8 value { 1 };
+    m8 addr1 { RDX };
+    m16 addr2 { RDX };
+    m32 addr3 { RDX };
+    m64 addr4 { RDX };
+    instr(AL, value);
+    instr(addr1, value);
+    instr(AL, CL);
+    instr(addr1, CL);
+    instr(AX, value);
+    instr(addr2, value);
+    instr(AX, CL);
+    instr(addr2, CL);
+    instr(EAX, value);
+    instr(addr3, value);
+    instr(EAX, CL);
+    instr(addr3, CL);
+    instr(RAX, value);
+    instr(addr4, value);
+    instr(RAX, CL);
+    instr(addr4, CL);
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -1932,6 +1957,15 @@ try
     gen_Instruction12_Type4(SHRD);
 
     gen_Instruction16_Type1(TEST);
+
+    gen_Instruction16_Type2(SAL);
+    gen_Instruction16_Type2(SAR);
+    gen_Instruction16_Type2(SHL);
+    gen_Instruction16_Type2(SHR);
+    gen_Instruction16_Type2(RCL);
+    gen_Instruction16_Type2(RCR);
+    gen_Instruction16_Type2(ROL);
+    gen_Instruction16_Type2(ROR);
 
     return EXIT_SUCCESS;
 }

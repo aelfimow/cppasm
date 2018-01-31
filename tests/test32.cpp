b49be18e47c891a0de2968ddd79b8a9481b66455
Alexander Elfimow <my.free.mail@gmx.de>
2018-01-31 21:07:34 +0100

    test32: added test for Instruction16_Type2

--- tests/test32.cpp
+++ tests/test32.cpp
@@ -971,6 +971,26 @@ static void gen_Instruction16_Type1(Instruction16_Type1 &instr)
     instr(addr3, EAX);
 }
 
+static void gen_Instruction16_Type2(Instruction16_Type2 &instr)
+{
+    imm8 value { 1 };
+    m8 addr1 { EDX };
+    m16 addr2 { EDX };
+    m32 addr3 { EDX };
+    instr(AL, value);
+    instr(addr1, value);
+    instr(AL, CL);
+    instr(addr1, CL);
+    instr(AX, value);
+    instr(addr2, value);
+    instr(AX, CL);
+    instr(addr2, CL);
+    instr(EAX, value);
+    instr(addr3, value);
+    instr(EAX, CL);
+    instr(addr3, CL);
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -1853,6 +1873,15 @@ try
     gen_Instruction12_Type4(SHRD);
 
     gen_Instruction16_Type1(TEST);
+
+    gen_Instruction16_Type2(SAL);
+    gen_Instruction16_Type2(SAR);
+    gen_Instruction16_Type2(SHL);
+    gen_Instruction16_Type2(SHR);
+    gen_Instruction16_Type2(RCL);
+    gen_Instruction16_Type2(RCR);
+    gen_Instruction16_Type2(ROL);
+    gen_Instruction16_Type2(ROR);
 
     return EXIT_SUCCESS;
 }

fc7a0a78f143e1cdd320c6456781ff28b9e8762e
Alexander Elfimow <my.free.mail@gmx.de>
2018-02-06 21:49:25 +0100

    test64: added test for Instruction26_Type1

--- tests/test64.cpp
+++ tests/test64.cpp
@@ -1075,6 +1075,43 @@ static void gen_Instruction16_Type2(Instruction16_Type2 &instr)
     instr(addr4, CL);
 }
 
+static void gen_Instruction26_Type1(Instruction26_Type1 &instr)
+{
+    imm8 value1 { 1 };
+    imm16 value2 { 2 };
+    imm32 value3 { 3 };
+    m8 addr1 { RDX };
+    m16 addr2 { RDX };
+    m32 addr3 { RDX };
+    m64 addr4 { RDX };
+    instr(AL, value1);
+    instr(addr1, value1);
+    instr(AX, value2);
+    instr(addr2, value2);
+    instr(EAX, value3);
+    instr(addr3, value3);
+    instr(RAX, value3);
+    instr(addr4, value3);
+    instr(AX, value1);
+    instr(addr2, value1);
+    instr(EAX, value1);
+    instr(addr3, value1);
+    instr(RAX, value1);
+    instr(addr4, value1);
+    instr(AL, BL);
+    instr(addr1, BL);
+    instr(AX, BX);
+    instr(addr2, BX);
+    instr(EAX, EBX);
+    instr(addr3, EBX);
+    instr(RAX, RCX);
+    instr(addr4, RCX);
+    instr(AL, addr1);
+    instr(AX, addr2);
+    instr(EAX, addr3);
+    instr(RAX, addr4);
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -1966,6 +2003,15 @@ try
     gen_Instruction16_Type2(RCR);
     gen_Instruction16_Type2(ROL);
     gen_Instruction16_Type2(ROR);
+
+    gen_Instruction26_Type1(ADD);
+    gen_Instruction26_Type1(ADC);
+    gen_Instruction26_Type1(SUB);
+    gen_Instruction26_Type1(SBB);
+    gen_Instruction26_Type1(CMP);
+    gen_Instruction26_Type1(AND);
+    gen_Instruction26_Type1(OR);
+    gen_Instruction26_Type1(XOR);
 
     return EXIT_SUCCESS;
 }

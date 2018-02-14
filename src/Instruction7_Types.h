4ebbf417a332fb0f465c02617b5e1f6bf7d2cba0
Alexander Elfimow <my.free.mail@gmx.de>
2018-02-14 21:52:43 +0100

    Fixes Instruction7_Type1 with indirect register and memory operands

--- src/Instruction7_Types.h
+++ src/Instruction7_Types.h
@@ -9,12 +9,12 @@
 using Instruction7_Type1 = Instruction_7
 <
     Operands_string,
-    Operands_reg<r16>,
-    Operands_mem<m16, no_suffix>,
-    Operands_reg<r32>,
-    Operands_mem<m32, no_suffix>,
-    Operands_reg<r64>,
-    Operands_mem<m64, no_suffix>
+    Operands_reg_indirect<r16>,
+    Operands_mem_indirect<m16, no_suffix>,
+    Operands_reg_indirect<r32>,
+    Operands_mem_indirect<m32, no_suffix>,
+    Operands_reg_indirect<r64>,
+    Operands_mem_indirect<m64, no_suffix>
 >;
 
 using Instruction7_Type2 = Instruction_7

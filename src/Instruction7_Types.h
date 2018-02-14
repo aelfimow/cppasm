f1f3219594afa0d4aefa0126d4727d3ad315c3bc
Alexander Elfimow <my.free.mail@gmx.de>
2018-02-14 21:59:30 +0100

    Added new instruction type Instruction7_Type3

--- src/Instruction7_Types.h
+++ src/Instruction7_Types.h
@@ -28,4 +28,15 @@ using Instruction7_Type2 = Instruction_7
     Operands_reg<Sreg>
 >;
 
+using Instruction7_Type3 = Instruction_7
+<
+    Operands_string_indirect,
+    Operands_reg_indirect<r16>,
+    Operands_mem_indirect<m16, no_suffix>,
+    Operands_reg_indirect<r32>,
+    Operands_mem_indirect<m32, no_suffix>,
+    Operands_reg_indirect<r64>,
+    Operands_mem_indirect<m64, no_suffix>
+>;
+
 #endif

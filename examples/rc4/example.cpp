7c6a9c1aa791b4ff2971c6edf5ec803d353d781a
Alexander Elfimow <my.free.mail@gmx.de>
2018-02-21 21:35:45 +0100

    Example rc4: added fix for proper labels

--- examples/rc4/example.cpp
+++ examples/rc4/example.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
 static_assert(sizeof(size_t) == 8, "size_t expected to be 64 bit");
 static_assert(sizeof(ssize_t) == 8, "ssize_t expected to be 64 bit");
+static_assert(sizeof(char) == 1, "char expected to be 8 bit");
 
 extern "C" void rc4init(const char key[], size_t L, char sbox[]); 
 extern "C" ssize_t rc4run(const char buf_in[], size_t len, char buf_out[], char sbox[]);
@@ -13,6 +16,13 @@ try
 {
     argc = argc;
     argv = argv;
+
+    const std::string key { "1234" };
+
+    constexpr size_t SBOX_SIZE = 256;
+    std::vector<char> sbox(SBOX_SIZE);
+
+    rc4init(key.c_str(), key.length(), sbox.data());
 
     return EXIT_SUCCESS;
 }
--- examples/rc4/generate.cpp
+++ examples/rc4/generate.cpp
@@ -26,20 +26,20 @@ static void gen_rc4init()
         imm64 init_values { 0x0706050403020100 };
         imm64 inc_values { 0x0808080808080808 };
 
-        r64 &inc_reg { R9 };
-        r64 &values_reg { R10 };
-        MOV(inc_reg, inc_values);
-        MOV(values_reg, init_values);
+        r64 &count_reg { RAX };
+        XOR(count_reg, count_reg);
 
         r64 &maxCount_reg { R11 };
         imm64 maxCount { (256 / 8) };
         MOV(maxCount_reg, maxCount);
 
-        r64 &count_reg { RAX };
-        XOR(count_reg, count_reg);
+        r64 &inc_reg { R9 };
+        r64 &values_reg { R10 };
+        MOV(inc_reg, inc_values);
+        MOV(values_reg, init_values);
 
-        std::string loop_start { "1" };
-        std::string loop_end   { "2" };
+        std::string loop_start { "loop1_s" };
+        std::string loop_end   { "loop1_e" };
         label(loop_start);
         {
             CMP(count_reg, maxCount_reg);
@@ -66,12 +66,11 @@ static void gen_rc4init()
         r8 &ii_reg { AL };
         XOR(i_reg, i_reg);
 
-        std::string loop_start("3");
+        std::string loop_start("loop2_s");
         label(loop_start);
         {
-
             INC(i_reg);
-            CMP(ii_reg, ii_reg);
+            TEST(ii_reg, ii_reg);
             JNZ(loop_start);
         }
     }

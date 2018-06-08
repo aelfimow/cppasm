e389a91059cc72330f637299870e7f3e33d00b47
Alexander Elfimow <my.free.mail@gmx.de>
2018-06-08 17:53:27 +0200

    xorshift-rng-2: use new functions in an example

--- examples/xorshift-rng-2/example.cpp
+++ examples/xorshift-rng-2/example.cpp
@@ -4,11 +4,32 @@
 
 #include "xorshift_func.h"
 
+static uint64_t make_triple(size_t a, size_t b, size_t c)
+{
+    const auto result = (1 * a) + (256 * b) + (65536 * c);
+
+    return result;
+}
+
 int main(int argc, char *argv[])
 try
 {
     argc = argc;
     argv = argv;
+
+    const auto triple = make_triple(1, 1, 54);
+
+    uint64_t state = 1;
+
+    state = xorshift_func1(triple, state);
+    state = xorshift_func2(triple, state);
+    state = xorshift_func3(triple, state);
+    state = xorshift_func4(triple, state);
+
+    if (state == 0)
+    {
+        throw std::runtime_error("state is 0");
+    }
 
     return EXIT_SUCCESS;
 }
--- examples/xorshift-rng-2/xorshift_func.h
+++ examples/xorshift-rng-2/xorshift_func.h
@@ -3,6 +3,9 @@
 #else
 #define XORSHIFT_FUNC_H
 
-extern "C" uint64_t xorshift_func(uint64_t triple, uint64_t rng_state);
+extern "C" uint64_t xorshift_func1(uint64_t triple, uint64_t rng_state);
+extern "C" uint64_t xorshift_func2(uint64_t triple, uint64_t rng_state);
+extern "C" uint64_t xorshift_func3(uint64_t triple, uint64_t rng_state);
+extern "C" uint64_t xorshift_func4(uint64_t triple, uint64_t rng_state);
 
 #endif

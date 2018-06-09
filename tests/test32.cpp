3b5eab797ae8a78f79de57181c927e2168eb2f53
Alexander Elfimow <my.free.mail@gmx.de>
2018-06-09 18:17:27 +0200

    test32: added test

--- tests/test32.cpp
+++ tests/test32.cpp
@@ -1494,6 +1494,7 @@ try
     gen_Instruction2_Type11(PMOVSXWQ);
     gen_Instruction2_Type11(PMOVZXBD);
     gen_Instruction2_Type11(PMOVZXWQ);
+    gen_Instruction2_Type11(RSQRTSS);
 
     gen_Instruction2_Type12(CMPPS);
     gen_Instruction2_Type12(SHUFPS);

117064fde410a4feee00d3df380f7d0ea1210cb1
Alexander Elfimow <my.free.mail@gmx.de>
2018-06-10 22:33:48 +0200

    cppasm: added further instruction types

--- src/Instruction5_Types.h
+++ src/Instruction5_Types.h
@@ -24,4 +24,40 @@ using Instruction5_Type2 = Instruction_5
     Operands_mem_reg_imm<m16, xmm, imm8>
 >;
 
+using Instruction5_Type3 = Instruction_5
+<
+    Operands_reg_reg<opmask_reg, opmask_reg, no_suffix>,
+    Operands_reg_mem<opmask_reg, m8, no_suffix>,
+    Operands_mem_reg<m8, opmask_reg, no_suffix>,
+    Operands_reg_reg<opmask_reg, r32, no_suffix>,
+    Operands_reg_reg<r32, opmask_reg, no_suffix>
+>;
+
+using Instruction5_Type4 = Instruction_5
+<
+    Operands_reg_reg<opmask_reg, opmask_reg, no_suffix>,
+    Operands_reg_mem<opmask_reg, m16, no_suffix>,
+    Operands_mem_reg<m16, opmask_reg, no_suffix>,
+    Operands_reg_reg<opmask_reg, r32, no_suffix>,
+    Operands_reg_reg<r32, opmask_reg, no_suffix>
+>;
+
+using Instruction5_Type5 = Instruction_5
+<
+    Operands_reg_reg<opmask_reg, opmask_reg, no_suffix>,
+    Operands_reg_mem<opmask_reg, m32, no_suffix>,
+    Operands_mem_reg<m32, opmask_reg, no_suffix>,
+    Operands_reg_reg<opmask_reg, r32, no_suffix>,
+    Operands_reg_reg<r32, opmask_reg, no_suffix>
+>;
+
+using Instruction5_Type6 = Instruction_5
+<
+    Operands_reg_reg<opmask_reg, opmask_reg, no_suffix>,
+    Operands_reg_mem<opmask_reg, m64, no_suffix>,
+    Operands_mem_reg<m64, opmask_reg, no_suffix>,
+    Operands_reg_reg<opmask_reg, r64, no_suffix>,
+    Operands_reg_reg<r64, opmask_reg, no_suffix>
+>;
+
 #endif

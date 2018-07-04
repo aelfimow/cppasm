3c9250ab841fe7153fc5b020a0720986a2f466ca
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-04 22:41:27 +0200

    test32: added file

---
+++ tests/test32/gen_Instruction10.cpp
@@ -0,0 +1,38 @@
+#include "cppasm.h"
+
+static void gen_Instruction10_Type1(Instruction10_Type1 &instr)
+{
+    instr(AX);
+    instr(EAX);
+    m16 addr1 { EDX };
+    m32 addr2 { EDX };
+    instr(addr1);
+    instr(addr2);
+    imm8 v1 { 0xAA };
+    imm16 v2 { 0xAABB };
+    imm32 v3 { 0xAABBCCDD };
+    instr(v1);
+    instr(v2);
+    instr(v3);
+    instr(GS);
+}
+
+static void gen_Instruction10_Type2(Instruction10_Type2 &instr)
+{
+    m8 addr1 { EDX };
+    m16 addr2 { EDX };
+    m32 addr3 { EDX };
+    instr(ECX, AL);
+    instr(ECX, addr1);
+    instr(ECX, AX);
+    instr(ECX, addr2);
+    instr(ECX, EAX);
+    instr(ECX, addr3);
+}
+
+void gen_Instruction10()
+{
+    gen_Instruction10_Type1(PUSH);
+
+    gen_Instruction10_Type2(CRC32);
+}

e6e1e2cd470e31d09d96fb618a22030cbe5b2837
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-04 22:39:11 +0200

    test32: use new function

--- tests/test32/test32.cpp
+++ tests/test32/test32.cpp
@@ -11,102 +11,7 @@ extern void gen_Instruction5();
 extern void gen_Instruction6();
 extern void gen_Instruction7();
 extern void gen_Instruction8();
-
-static void gen_Instruction9_Type1(Instruction9_Type1 &instr)
-{
-    m8 addr { EBX };
-    instr(XMM0, EAX);
-    instr(YMM0, EAX);
-    instr(ZMM0, EAX);
-    instr(XMM0.k1, EAX);
-    instr(YMM0.k1, EAX);
-    instr(ZMM0.k1, EAX);
-    instr(XMM0.k1.z, EAX);
-    instr(YMM0.k1.z, EAX);
-    instr(ZMM0.k1.z, EAX);
-    instr(XMM0, XMM0);
-    instr(YMM0, XMM0);
-    instr(ZMM0, XMM0);
-    instr(XMM0.k1, XMM0);
-    instr(YMM0.k1, XMM0);
-    instr(ZMM0.k1, XMM0);
-    instr(XMM0.k1.z, XMM0);
-    instr(YMM0.k1.z, XMM0);
-    instr(ZMM0.k1.z, XMM0);
-    instr(XMM0, addr);
-    instr(YMM0, addr);
-    instr(ZMM0, addr);
-    instr(XMM0.k1, addr);
-    instr(YMM0.k1, addr);
-    instr(ZMM0.k1, addr);
-    instr(XMM0.k1.z, addr);
-    instr(YMM0.k1.z, addr);
-    instr(ZMM0.k1.z, addr);
-}
-
-static void gen_Instruction9_Type2(Instruction9_Type2 &instr)
-{
-    m16 addr { EBX };
-    instr(XMM0, EAX);
-    instr(YMM0, EAX);
-    instr(ZMM0, EAX);
-    instr(XMM0.k1, EAX);
-    instr(YMM0.k1, EAX);
-    instr(ZMM0.k1, EAX);
-    instr(XMM0.k1.z, EAX);
-    instr(YMM0.k1.z, EAX);
-    instr(ZMM0.k1.z, EAX);
-    instr(XMM0, XMM0);
-    instr(YMM0, XMM0);
-    instr(ZMM0, XMM0);
-    instr(XMM0.k1, XMM0);
-    instr(YMM0.k1, XMM0);
-    instr(ZMM0.k1, XMM0);
-    instr(XMM0.k1.z, XMM0);
-    instr(YMM0.k1.z, XMM0);
-    instr(ZMM0.k1.z, XMM0);
-    instr(XMM0, addr);
-    instr(YMM0, addr);
-    instr(ZMM0, addr);
-    instr(XMM0.k1, addr);
-    instr(YMM0.k1, addr);
-    instr(ZMM0.k1, addr);
-    instr(XMM0.k1.z, addr);
-    instr(YMM0.k1.z, addr);
-    instr(ZMM0.k1.z, addr);
-}
-
-static void gen_Instruction9_Type3(Instruction9_Type3 &instr)
-{
-    m32 addr { EBX };
-    instr(XMM0, EAX);
-    instr(YMM0, EAX);
-    instr(ZMM0, EAX);
-    instr(XMM0.k1, EAX);
-    instr(YMM0.k1, EAX);
-    instr(ZMM0.k1, EAX);
-    instr(XMM0.k1.z, EAX);
-    instr(YMM0.k1.z, EAX);
-    instr(ZMM0.k1.z, EAX);
-    instr(XMM0, XMM0);
-    instr(YMM0, XMM0);
-    instr(ZMM0, XMM0);
-    instr(XMM0.k1, XMM0);
-    instr(YMM0.k1, XMM0);
-    instr(ZMM0.k1, XMM0);
-    instr(XMM0.k1.z, XMM0);
-    instr(YMM0.k1.z, XMM0);
-    instr(ZMM0.k1.z, XMM0);
-    instr(XMM0, addr);
-    instr(YMM0, addr);
-    instr(ZMM0, addr);
-    instr(XMM0.k1, addr);
-    instr(YMM0.k1, addr);
-    instr(ZMM0.k1, addr);
-    instr(XMM0.k1.z, addr);
-    instr(YMM0.k1.z, addr);
-    instr(ZMM0.k1.z, addr);
-}
+extern void gen_Instruction9();
 
 static void gen_Instruction10_Type1(Instruction10_Type1 &instr)
 {
@@ -319,10 +224,7 @@ try
     gen_Instruction6();
     gen_Instruction7();
     gen_Instruction8();
-
-    gen_Instruction9_Type1(VPBROADCASTB);
-    gen_Instruction9_Type2(VPBROADCASTW);
-    gen_Instruction9_Type3(VPBROADCASTD);
+    gen_Instruction9();
 
     gen_Instruction10_Type1(PUSH);
 

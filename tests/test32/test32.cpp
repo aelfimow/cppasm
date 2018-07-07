84ad9836d475e3475e88230e47f063909bef1fa4
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-07 22:32:54 +0200

    test32: use new function

--- tests/test32/test32.cpp
+++ tests/test32/test32.cpp
@@ -13,64 +13,7 @@ extern void gen_Instruction7();
 extern void gen_Instruction8();
 extern void gen_Instruction9();
 extern void gen_Instruction10();
-
-static void gen_Instruction12_Type1(Instruction12_Type1 &instr)
-{
-    m16 addr1 { EDX };
-    m32 addr2 { EDX };
-    imm8 value { 1 };
-    instr(AX, BX);
-    instr(addr1, AX);
-    instr(EAX, EBX);
-    instr(addr2, EAX);
-    instr(AX, value);
-    instr(EAX, value);
-    instr(addr1, value);
-    instr(addr2, value);
-}
-
-static void gen_Instruction12_Type2(Instruction12_Type2 &instr)
-{
-    m8 addr1 { EDX };
-    m16 addr2 { EDX };
-    m32 addr3 { EDX };
-    instr(AL, BL);
-    instr(addr1, CL);
-    instr(CL, addr1);
-    instr(AX, BX);
-    instr(addr2, CX);
-    instr(CX, addr2);
-    instr(EAX, EBX);
-    instr(addr3, ECX);
-    instr(ECX, addr3);
-}
-
-static void gen_Instruction12_Type3(Instruction12_Type3 &instr)
-{
-    m8 addr1 { EDX };
-    m16 addr2 { EDX };
-    instr(AX, BL);
-    instr(AX, addr1);
-    instr(EAX, BL);
-    instr(EAX, addr1);
-    instr(EAX, AX);
-    instr(EAX, addr2);
-}
-
-static void gen_Instruction12_Type4(Instruction12_Type4 &instr)
-{
-    imm8 value { 1 };
-    m16 addr1 { EDX };
-    m32 addr2 { EDX };
-    instr(AX, BX, value);
-    instr(addr1, BX, value);
-    instr(AX, BX, CL);
-    instr(addr1, BX, CL);
-    instr(EAX, EBX, value);
-    instr(addr2, EBX, value);
-    instr(EAX, EBX, CL);
-    instr(addr2, EBX, CL);
-}
+extern void gen_Instruction12();
 
 static void gen_Instruction16_Type1(Instruction16_Type1 &instr)
 {
@@ -197,19 +140,7 @@ try
     gen_Instruction8();
     gen_Instruction9();
     gen_Instruction10();
-
-    gen_Instruction12_Type1(BT);
-    gen_Instruction12_Type1(BTC);
-    gen_Instruction12_Type1(BTR);
-    gen_Instruction12_Type1(BTS);
-
-    gen_Instruction12_Type2(XCHG);
-
-    gen_Instruction12_Type3(MOVSX);
-    gen_Instruction12_Type3(MOVZX);
-
-    gen_Instruction12_Type4(SHLD);
-    gen_Instruction12_Type4(SHRD);
+    gen_Instruction12();
 
     gen_Instruction16_Type1(TEST);
 

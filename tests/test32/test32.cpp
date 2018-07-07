103acfab01a928918856049f4d5f4279e6f6bc26
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-07 22:35:38 +0200

    test32: use new function

--- tests/test32/test32.cpp
+++ tests/test32/test32.cpp
@@ -14,48 +14,7 @@ extern void gen_Instruction8();
 extern void gen_Instruction9();
 extern void gen_Instruction10();
 extern void gen_Instruction12();
-
-static void gen_Instruction16_Type1(Instruction16_Type1 &instr)
-{
-    imm8 value1 { 0xAA };
-    imm16 value2 { 0xAABB };
-    imm32 value3 { 0xAABBCCDD };
-    m8 addr1 { EDX };
-    m16 addr2 { EDX };
-    m32 addr3 { EDX };
-    instr(AL, value1);
-    instr(addr1, value1);
-    instr(AX, value2);
-    instr(addr2, value2);
-    instr(EAX, value3);
-    instr(addr3, value3);
-    instr(AL, AH);
-    instr(addr1, AL);
-    instr(AX, BX);
-    instr(addr2, AX);
-    instr(EAX, EBX);
-    instr(addr3, EAX);
-}
-
-static void gen_Instruction16_Type2(Instruction16_Type2 &instr)
-{
-    imm8 value { 1 };
-    m8 addr1 { EDX };
-    m16 addr2 { EDX };
-    m32 addr3 { EDX };
-    instr(AL, value);
-    instr(addr1, value);
-    instr(AL, CL);
-    instr(addr1, CL);
-    instr(AX, value);
-    instr(addr2, value);
-    instr(AX, CL);
-    instr(addr2, CL);
-    instr(EAX, value);
-    instr(addr3, value);
-    instr(EAX, CL);
-    instr(addr3, CL);
-}
+extern void gen_Instruction16();
 
 static void gen_Instruction26_Type1(Instruction26_Type1 &instr)
 {
@@ -141,17 +100,7 @@ try
     gen_Instruction9();
     gen_Instruction10();
     gen_Instruction12();
-
-    gen_Instruction16_Type1(TEST);
-
-    gen_Instruction16_Type2(SAL);
-    gen_Instruction16_Type2(SAR);
-    gen_Instruction16_Type2(SHL);
-    gen_Instruction16_Type2(SHR);
-    gen_Instruction16_Type2(RCL);
-    gen_Instruction16_Type2(RCR);
-    gen_Instruction16_Type2(ROL);
-    gen_Instruction16_Type2(ROR);
+    gen_Instruction16();
 
     gen_Instruction26_Type1(ADD);
     gen_Instruction26_Type1(ADC);

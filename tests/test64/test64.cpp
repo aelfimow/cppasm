fd5bf62bd0567b723344746dba346ba7c4b5e7cd
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-12 23:03:58 +0200

    test64: use new function

--- tests/test64/test64.cpp
+++ tests/test64/test64.cpp
@@ -14,58 +14,7 @@ extern void gen_Instruction8();
 extern void gen_Instruction9();
 extern void gen_Instruction10();
 extern void gen_Instruction12();
-
-static void gen_Instruction16_Type1(Instruction16_Type1 &instr)
-{
-    imm8 value1 { 0xAA };
-    imm16 value2 { 0xAABB };
-    imm32 value3 { 0xAABBCCDD };
-    m8 addr1 { RDX };
-    m16 addr2 { RDX };
-    m32 addr3 { RDX };
-    m64 addr4 { RDX };
-    instr(AL, value1);
-    instr(addr1, value1);
-    instr(AX, value2);
-    instr(addr2, value2);
-    instr(EAX, value3);
-    instr(addr3, value3);
-    //instr(RAX, value3);
-    //instr(addr4, value3);
-    instr(AL, AH);
-    instr(addr1, AL);
-    instr(AX, BX);
-    instr(addr2, AX);
-    instr(EAX, EBX);
-    instr(addr3, EAX);
-    instr(RAX, RBX);
-    instr(addr4, RAX);
-}
-
-static void gen_Instruction16_Type2(Instruction16_Type2 &instr)
-{
-    imm8 value { 1 };
-    m8 addr1 { RDX };
-    m16 addr2 { RDX };
-    m32 addr3 { RDX };
-    m64 addr4 { RDX };
-    instr(AL, value);
-    instr(addr1, value);
-    instr(AL, CL);
-    instr(addr1, CL);
-    instr(AX, value);
-    instr(addr2, value);
-    instr(AX, CL);
-    instr(addr2, CL);
-    instr(EAX, value);
-    instr(addr3, value);
-    instr(EAX, CL);
-    instr(addr3, CL);
-    instr(RAX, value);
-    instr(addr4, value);
-    instr(RAX, CL);
-    instr(addr4, CL);
-}
+extern void gen_Instruction16();
 
 static void gen_Instruction26_Type1(Instruction26_Type1 &instr)
 {
@@ -170,17 +119,7 @@ try
     gen_Instruction9();
     gen_Instruction10();
     gen_Instruction12();
-
-    gen_Instruction16_Type1(TEST);
-
-    gen_Instruction16_Type2(SAL);
-    gen_Instruction16_Type2(SAR);
-    gen_Instruction16_Type2(SHL);
-    gen_Instruction16_Type2(SHR);
-    gen_Instruction16_Type2(RCL);
-    gen_Instruction16_Type2(RCR);
-    gen_Instruction16_Type2(ROL);
-    gen_Instruction16_Type2(ROR);
+    gen_Instruction16();
 
     gen_Instruction26_Type1(ADD);
     gen_Instruction26_Type1(ADC);

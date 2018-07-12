de2e0ce677cb8117243fd076e119fe2816332e16
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-12 23:02:05 +0200

    test64: added file

---
+++ tests/test64/gen_Instruction12.cpp
@@ -0,0 +1,93 @@
+#include "cppasm.h"
+
+static void gen_Instruction12_Type1(Instruction12_Type1 &instr)
+{
+    m16 addr1 { RDX };
+    m32 addr2 { RDX };
+    m64 addr3 { RDX };
+    imm8 value { 1 };
+    instr(AX, BX);
+    instr(addr1, AX);
+    instr(EAX, EBX);
+    instr(addr2, EAX);
+    instr(RAX, RBX);
+    instr(addr3, RAX);
+    instr(AX, value);
+    instr(EAX, value);
+    instr(RAX, value);
+    instr(addr1, value);
+    instr(addr2, value);
+    instr(addr3, value);
+}
+
+static void gen_Instruction12_Type2(Instruction12_Type2 &instr)
+{
+    m8 addr1 { RDX };
+    m16 addr2 { RDX };
+    m32 addr3 { RDX };
+    m64 addr4 { RDX };
+    instr(AL, BL);
+    instr(addr1, CL);
+    instr(CL, addr1);
+    instr(AX, BX);
+    instr(addr2, CX);
+    instr(CX, addr2);
+    instr(EAX, EBX);
+    instr(addr3, ECX);
+    instr(ECX, addr3);
+    instr(RAX, RBX);
+    instr(addr4, RCX);
+    instr(RCX, addr4);
+}
+
+static void gen_Instruction12_Type3(Instruction12_Type3 &instr)
+{
+    m8 addr1 { RDX };
+    m16 addr2 { RDX };
+    instr(AX, BL);
+    instr(AX, addr1);
+    instr(EAX, BL);
+    instr(EAX, addr1);
+    instr(RAX, BL);
+    instr(RAX, addr1);
+    instr(EAX, AX);
+    instr(EAX, addr2);
+    instr(RAX, BX);
+    instr(RAX, addr2);
+}
+
+static void gen_Instruction12_Type4(Instruction12_Type4 &instr)
+{
+    imm8 value { 1 };
+    m16 addr1 { RDX };
+    m32 addr2 { RDX };
+    m64 addr3 { RDX };
+    instr(AX, BX, value);
+    instr(addr1, BX, value);
+    instr(AX, BX, CL);
+    instr(addr1, BX, CL);
+    instr(EAX, EBX, value);
+    instr(addr2, EBX, value);
+    instr(RAX, RBX, value);
+    instr(addr3, RBX, value);
+    instr(EAX, EBX, CL);
+    instr(addr2, EBX, CL);
+    instr(RAX, RBX, CL);
+    instr(addr3, RBX, CL);
+}
+
+void gen_Instruction12()
+{
+    gen_Instruction12_Type1(BT);
+    gen_Instruction12_Type1(BTC);
+    gen_Instruction12_Type1(BTR);
+    gen_Instruction12_Type1(BTS);
+
+    gen_Instruction12_Type2(XCHG);
+
+    gen_Instruction12_Type3(MOVSX);
+    gen_Instruction12_Type3(MOVZX);
+
+    gen_Instruction12_Type4(SHLD);
+    gen_Instruction12_Type4(SHRD);
+}

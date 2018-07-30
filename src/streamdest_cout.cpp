fe88dd7eebcb8edaa66340048615e208b865732f
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-30 21:57:22 +0200

    cppasm: added files

---
+++ src/streamdest_cout.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "streamdest.h"
+#include "streamdest_cout.h"
+
+streamdest_cout::streamdest_cout()
+{
+}
+
+streamdest_cout::~streamdest_cout()
+{
+}
+
+void streamdest_cout::space()
+{
+    std::cout << ' ';
+}
+
+void streamdest_cout::indent()
+{
+    std::cout << '\t';
+}
+
+void streamdest_cout::writeln(const std::string &str)
+{
+    std::cout << str.c_str() << std::endl;
+}
---
+++ src/streamdest_cout.h
@@ -0,0 +1,27 @@
+/*! \file streamdest_cout.h
+    \brief Class for Assembly Stream destination std::cout.
+*/
+#ifdef STREAM_DEST_COUT_H
+#error Already included
+#else
+#define STREAM_DEST_COUT_H
+
+/*! \class Assembly stream destination std::cout */
+class streamdest_cout: public streamdest
+{
+    public:
+        streamdest_cout();
+        ~streamdest_cout();
+
+        void space() override;
+        void indent() override;
+        void writeln(const std::string &str) override;
+
+    public:
+        streamdest_cout(const streamdest_cout &instance) = delete;
+        streamdest_cout(const streamdest_cout &&instance) = delete;
+        streamdest_cout &operator=(const streamdest_cout &instance) = delete;
+        streamdest_cout &operator=(const streamdest_cout &&instance) = delete;
+};
+
+#endif

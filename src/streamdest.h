33bd32bb1718ba31f771ae6a50e866a677b4942e
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-30 21:40:47 +0200

    cppasm: added interface class for stream destination

---
+++ src/streamdest.h
@@ -0,0 +1,27 @@
+/*! \file streamdest.h
+    \brief Class for Assembly Stream destination.
+*/
+#ifdef STREAM_DEST_H
+#error Already included
+#else
+#define STREAM_DEST_H
+
+/*! \class Assembly stream destination */
+class streamdest
+{
+    public:
+        streamdest() { }
+        virtual ~streamdest() { }
+
+        virtual void space() = 0;
+        virtual void indent() = 0;
+        virtual void writeln(const std::string &str) = 0;
+
+    public:
+        streamdest(const streamdest &instance) = delete;
+        streamdest(const streamdest &&instance) = delete;
+        streamdest &operator=(const streamdest &instance) = delete;
+        streamdest &operator=(const streamdest &&instance) = delete;
+};
+
+#endif

0a381a84fcb12b66d391a468b877862e653de739
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-27 21:26:38 +0200

    macheps: generate first function

--- examples/macheps/generate.cpp
+++ examples/macheps/generate.cpp
@@ -4,6 +4,18 @@
 
 #include "cppasm.h"
 
+using fp32_t = float;
+
+struct fp32_params
+{
+    fp32_t value;
+    fp32_t epsilon;
+    fp32_t sum;
+};
+
+static_assert(sizeof(fp32_t) == 4);
+static_assert(sizeof(fp32_params) == (3 * sizeof(fp32_t)));
+
 int main(int argc, char *argv[])
 try
 {
@@ -12,6 +24,33 @@ try
 
     section code { ".text" };
     code.start();
+
+    const std::string func_name { "macheps_fp32_init" };
+    comment("void " + func_name + "(void *param)");
+    comment("param is in %rcx");
+
+    global(func_name);
+    label(func_name);
+
+    m32fp value { RCX };
+    value.disp(offsetof(struct fp32_params, value));
+
+    m32fp epsilon { RCX };
+    epsilon.disp(offsetof(struct fp32_params, epsilon));
+
+    m32fp sum { RCX };
+    sum.disp(offsetof(struct fp32_params, sum));
+
+    FLD1();
+    FST(value);
+    FSTP(epsilon);
+
+    FLD(value);
+    FLD(epsilon);
+
+    FADDP();
+
+    FSTP(sum);
 
     RET();
 

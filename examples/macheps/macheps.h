815fbfd9bcea5657fcd19d2bacc502811200d97b
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-28 22:48:11 +0200

    Example macheps: added types for double usage

--- examples/macheps/macheps.h
+++ examples/macheps/macheps.h
@@ -4,8 +4,10 @@
 #define MACH_EPS_H
 
 using fp32_t = float;
+using fp64_t = double;
 
 static_assert(sizeof(fp32_t) == 4);
+static_assert(sizeof(fp64_t) == 8);
 
 template <typename T> struct fp_params
 {
@@ -15,10 +17,15 @@ template <typename T> struct fp_params
 };
 
 using fp32_params = struct fp_params<fp32_t>;
+using fp64_params = struct fp_params<fp64_t>;
 
 static_assert(sizeof(fp32_params) == (3 * sizeof(fp32_t)));
+static_assert(sizeof(fp64_params) == (3 * sizeof(fp64_t)));
 
 extern "C" void macheps_fp32_init(void *param);
 extern "C" void macheps_fp32_compute(void *param);
+
+extern "C" void macheps_fp64_init(void *param);
+extern "C" void macheps_fp64_compute(void *param);
 
 #endif

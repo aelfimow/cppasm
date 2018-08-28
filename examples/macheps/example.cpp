d82cf1440a28042824b9b0927425606d37a65e50
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-28 22:56:07 +0200

    Example macheps: simplify fp32_params print

--- examples/macheps/example.cpp
+++ examples/macheps/example.cpp
@@ -4,6 +4,17 @@
 
 #include "macheps.h"
 
+std::ostream &operator<<(std::ostream &os, const fp32_params &param)
+{
+    os << "value: " << param.value;
+    os << "; ";
+    os << "epsilon: " << param.epsilon;
+    os << "; ";
+    os << "sum: " << param.sum;
+
+    return os;
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -13,18 +24,13 @@ try
     fp32_params param;
     macheps_fp32_init(&param);
 
-    std::cout << "Init: " <<
-        "value: " << param.value << "; " <<
-        "epsilon: " << param.epsilon << "; " <<
-        "sum: " << param.sum << std::endl;
+    std::cout << "Init: " << param << std::endl;
 
     for (auto cnt = 0; cnt < 10; ++cnt)
     {
         macheps_fp32_compute(&param);
-        std::cout << "Compute: " <<
-            "value: " << param.value << "; " <<
-            "epsilon: " << param.epsilon << "; " <<
-            "sum: " << param.sum << std::endl;
+
+        std::cout << "Compute: " << param << std::endl;
     }
 
     return EXIT_SUCCESS;

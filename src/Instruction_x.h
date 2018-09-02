2c52f3b8ca959bcf635f73633ed2619795ce3deb
Alexander Elfimow <my.free.mail@gmx.de>
2018-09-02 21:23:19 +0200

    cppasm: replaced Instruction_4 with Instruction_x

--- src/Instruction_x.h
+++ src/Instruction_x.h
@@ -29,36 +29,6 @@ template
     class T1,
     class T2,
     class T3,
-    class T4
->
-class Instruction_4 :
-    public T1,
-    public T2,
-    public T3,
-    public T4
-{
-    public:
-        Instruction_4(asmstream &s, const std::string mnem) :
-            T1 { s, mnem },
-            T2 { s, mnem },
-            T3 { s, mnem },
-            T4 { s, mnem }
-        {
-        }
-
-        ~Instruction_4() { }
-
-        using T1::operator();
-        using T2::operator();
-        using T3::operator();
-        using T4::operator();
-};
-
-template
-<
-    class T1,
-    class T2,
-    class T3,
     class T4,
     class T5
 >

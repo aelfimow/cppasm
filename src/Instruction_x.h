6ae648b0e582933d2c6637b67f617013fb6f96f7
Alexander Elfimow <my.free.mail@gmx.de>
2018-09-02 21:47:00 +0200

    cppasm: replaced Instruction_16 with Instruction_x

--- src/Instruction_x.h
+++ src/Instruction_x.h
@@ -1,5 +1,5 @@
 /*! \file Instruction_x.h
-    \brief A set of template classes representing instructions.
+    \brief A template class representing instructions.
 */
 #ifdef INSTRUCTION_X_H
 #error Already included
@@ -22,84 +22,6 @@ class Instruction_x:
         ~Instruction_x() { }
 
         using Ts::operator() ...;
-};
-
-template
-<
-    class T1,
-    class T2,
-    class T3,
-    class T4,
-    class T5,
-    class T6,
-    class T7,
-    class T8,
-    class T9,
-    class T10,
-    class T11,
-    class T12,
-    class T13,
-    class T14,
-    class T15,
-    class T16
->
-class Instruction_16 :
-    public T1,
-    public T2,
-    public T3,
-    public T4,
-    public T5,
-    public T6,
-    public T7,
-    public T8,
-    public T9,
-    public T10,
-    public T11,
-    public T12,
-    public T13,
-    public T14,
-    public T15,
-    public T16
-{
-    public:
-        Instruction_16(asmstream &s, const std::string mnem) :
-            T1 { s, mnem },
-            T2 { s, mnem },
-            T3 { s, mnem },
-            T4 { s, mnem },
-            T5 { s, mnem },
-            T6 { s, mnem },
-            T7 { s, mnem },
-            T8 { s, mnem },
-            T9 { s, mnem },
-            T10 { s, mnem },
-            T11 { s, mnem },
-            T12 { s, mnem },
-            T13 { s, mnem },
-            T14 { s, mnem },
-            T15 { s, mnem },
-            T16 { s, mnem }
-        {
-        }
-
-        ~Instruction_16() { }
-
-        using T1::operator();
-        using T2::operator();
-        using T3::operator();
-        using T4::operator();
-        using T5::operator();
-        using T6::operator();
-        using T7::operator();
-        using T8::operator();
-        using T9::operator();
-        using T10::operator();
-        using T11::operator();
-        using T12::operator();
-        using T13::operator();
-        using T14::operator();
-        using T15::operator();
-        using T16::operator();
 };
 
 #endif

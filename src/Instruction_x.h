3c61c0f335e37704ad8f1b376c9642daad953559
Alexander Elfimow <my.free.mail@gmx.de>
2018-09-05 20:26:33 +0200

    cppasm: introduced Instruction_9 alias

--- src/Instruction_x.h
+++ src/Instruction_x.h
@@ -152,4 +152,29 @@ using Instruction_8 = Instruction_x
     Op8
 >;
 
+template
+<
+    class Op1,
+    class Op2,
+    class Op3,
+    class Op4,
+    class Op5,
+    class Op6,
+    class Op7,
+    class Op8,
+    class Op9
+>
+using Instruction_9 = Instruction_x
+<
+    Op1,
+    Op2,
+    Op3,
+    Op4,
+    Op5,
+    Op6,
+    Op7,
+    Op8,
+    Op9
+>;
+
 #endif

bf85a987e444151fbd0defe392ee23a28b01b3a4
Alexander Elfimow <my.free.mail@gmx.de>
2018-09-15 21:59:12 +0200

    cppasm: added type

--- src/Instruction9_Types.h
+++ src/Instruction9_Types.h
@@ -84,4 +84,17 @@ using Instruction9_Type6 = Instruction_9
     Operands_reg_reg_mem_imm<zmm, zmm, m64, imm8>
 >;
 
+using Instruction9_Type7 = Instruction_9
+<
+    Operands_reg_reg_reg<xmm, xmm, xmm>,
+    Operands_reg_reg_mem<xmm, xmm, m128>,
+    Operands_reg_reg_mem<xmm, xmm, m32>,
+    Operands_reg_reg_reg<ymm, ymm, ymm>,
+    Operands_reg_reg_mem<ymm, ymm, m256>,
+    Operands_reg_reg_mem<ymm, ymm, m32>,
+    Operands_reg_reg_reg<zmm, zmm, zmm>,
+    Operands_reg_reg_mem<zmm, zmm, m512>,
+    Operands_reg_reg_mem<zmm, zmm, m32>
+>;
+
 #endif

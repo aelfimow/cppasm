55322c594a99ff792fa9c18269a4ea1ae350611c
Alexander Elfimow <my.free.mail@gmx.de>
2019-03-12 22:16:31 +0100

    clflush: generate function for windows or linux

--- examples/clflush/generate.cpp
+++ examples/clflush/generate.cpp
@@ -11,22 +11,16 @@ struct reg_usage
     r64 &tmp;
 };
 
-int main(int argc, char *argv[])
-try
+static void usage()
 {
-    argc = argc;
-    argv = argv;
+    std::cerr << "Specify: windows or linux" << std::endl;
+}
 
+static void generate(struct reg_usage &regs)
+{
     // Function name to be generated
     std::string const func_name { "clflush_func" };
     comment("void " + func_name + "(void *p1, void *p2)");
-
-    struct reg_usage regs =
-    {
-        RCX,
-        RDX,
-        RAX
-    };
 
     comment("p1 is in " + regs.param1.name());
     comment("p2 is in " + regs.param2.name());
@@ -53,6 +47,41 @@ try
     CLFLUSH(p2);
 
     RET();
+}
+
+int main(int argc, char *argv[])
+try
+{
+    if (argc != 2)
+    {
+        usage();
+        return EXIT_FAILURE;
+    }
+
+    std::string const argstr { argv[1] };
+
+    bool const is_windows = (argstr == "windows");
+    bool const is_linux = (argstr == "linux");
+
+    if (!is_windows && !is_linux)
+    {
+        usage();
+        return EXIT_FAILURE;
+    }
+
+    if (is_windows)
+    {
+        struct reg_usage regs = { RCX, RDX, RAX };
+
+        generate(regs);
+    }
+
+    if (is_linux)
+    {
+        struct reg_usage regs = { RDI, RSI, RAX };
+
+        generate(regs);
+    }
 
     return EXIT_SUCCESS;
 }

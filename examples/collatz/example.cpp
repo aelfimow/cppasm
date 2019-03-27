968d6d166bb9827d690c50ba81f4f4c80282c8c3
Alexander Elfimow <my.free.mail@gmx.de>
2019-03-27 22:09:28 +0100

    collatz: use new function

--- examples/collatz/example.cpp
+++ examples/collatz/example.cpp
@@ -5,6 +5,7 @@
 static_assert(sizeof(size_t) == 8, "size_t expected to be 64 bit");
 
 extern "C" size_t collatz_length(size_t value);
+extern "C" size_t collatz_calc(size_t value);
 
 int main(int argc, char *argv[])
 try
@@ -17,6 +18,22 @@ try
     if (len != 20)
     {
         std::cerr << "Error, unexpected length: " << len << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    auto result = collatz_calc(1);
+
+    if (result != 4)
+    {
+        std::cerr << "Error, unexpected result (1): " << result << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    result = collatz_calc(2);
+
+    if (result != 1)
+    {
+        std::cerr << "Error, unexpected result (2): " << result << std::endl;
         return EXIT_FAILURE;
     }
 

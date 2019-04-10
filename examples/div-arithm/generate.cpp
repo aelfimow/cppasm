cb8aabf1e284b7caab5a28ae41b925cb694b3a5b
Alexander Elfimow <my.free.mail@gmx.de>
2019-04-10 22:07:58 +0200

    div-arithm: implemented generator

--- examples/div-arithm/generate.cpp
+++ examples/div-arithm/generate.cpp
@@ -3,6 +3,8 @@
 #include <stdexcept>
 
 #include "cppasm.h"
+#include "div_arithm.h"
+
 
 int main(int argc, char *argv[])
 try
@@ -21,6 +23,32 @@ try
     global(func_name);
 
     label(func_name);
+
+    r64 &param_reg { RCX };
+
+    m64 Z1_addr;
+    Z1_addr.base(param_reg).disp(offsetof(struct div_param, Z1));
+
+    m64 Z2_addr;
+    Z2_addr.base(param_reg).disp(offsetof(struct div_param, Z2));
+
+    m64 N_addr;
+    N_addr.base(param_reg).disp(offsetof(struct div_param, N));
+
+    m64 div_result_addr;
+    div_result_addr.base(param_reg).disp(offsetof(struct div_param, div_result));
+
+    m64 mod_result_addr;
+    mod_result_addr.base(param_reg).disp(offsetof(struct div_param, mod_result));
+
+    MOV(RAX, Z1_addr);
+    MOV(RDX, Z2_addr);
+    MOV(R9, N_addr);
+
+    DIV(R9);
+
+    MOV(div_result_addr, RAX);
+    MOV(mod_result_addr, RDX);
 
     RET();
 
